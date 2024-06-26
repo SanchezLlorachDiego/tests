c9b96dc72ede8d6ccc9f97062b9a089761f8951b
GGarrido <g.garridoportes@edu.gva.es>
2024-06-26 22:04:12 +0200

    Update SolutionTest.php

--- tests/SolutionTest.php
+++ tests/SolutionTest.php
@@ -4,15 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 class SolutionTest extends TestCase
 {
-    public function testPrintMessage() {
-        // Capturamos la salida de la función
-        ob_start();
-        printMessage();
-        $output = ob_get_clean();
 
-        // Verificamos que la salida sea la esperada
-        $this->assertEquals('Hello, World!', $output);
-    }
 }
 ?>
  

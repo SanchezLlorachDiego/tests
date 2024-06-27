601ed1bb61fbb09aa46790b2e3546022419735fc
SanchezLlorachDiego <d.sanchezllorach@edu.gva.es>
2024-06-27 12:34:29 +0200

    Update SolutionTest.php

--- tests/SolutionTest.php
+++ tests/SolutionTest.php
@@ -4,6 +4,14 @@ use PHPUnit\Framework\TestCase;
 
 class SolutionTest extends TestCase
 {
+ public function testSuma() {
+
+     $numero = 5;
+     $numero2 = 10;
+
+     $this->assertEquals(15, suma($numero, $numero2));
+  
+ }
 
 }
 ?>
